d429da7bc19d3d03bf49cee60c7f24d893d8bbd4
Jedidiah Foster <jedidiah.foster@gmail.com>
2022-06-30 11:24:15 -0700

    Testing updates

--- test/integration/default/controls/test_data.rb
+++ test/integration/default/controls/test_data.rb
@@ -3,8 +3,10 @@
 include_controls 'inspec-aws'
 require './test/library/common'
 
+tfstate = StateFileReader.new
+
 control 'default' do
-  describe json(state_file) do
+  describe json(tfstate.state_file_path) do
     its(%w[outputs ami-kms-arn value ami_id]) { should eq 'ami-052efd3df9dad4825' }
     its(%w[outputs ami-kms-arn value kms_arn]) { should eq '' }
     its(%w[outputs ami-kms-arn value snapshot_id]) { should eq 'snap-02d9369affc74b4f8' }
